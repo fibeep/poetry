64e01e4a093225e69ee5032c594fa4aab312ec6c
Salo <salomonco@gmail.com>
2020-09-25 18:28:20 -0400

    prints backwards!

--- slam.py
+++ slam.py
@@ -9,3 +9,11 @@ def get_file_lines(filename):
 
 
 lines_list = get_file_lines("poem.txt")
+
+# Prints the poem backwards as well as their initial starting position
+def lines_printed_backwards(lines_list):
+    for i in range(len(lines_list)):
+        print(len(lines_list) - (i), lines_list[-(i + 1)])
+
+
+lines_printed_backwards(lines_list)
