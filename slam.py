f31c98c786e9128860d9267bcc31c30a0e3209f4
Salo <salomonco@gmail.com>
2020-09-27 12:09:26 -0400

    documented custom function

--- slam.py
+++ slam.py
@@ -27,16 +27,19 @@ def lines_printed_random(lines_list):
 
 lines_printed_random(lines_list)
 
+#This custom function will determine whether the text 
+#document has an even or odd number of lines, and then
+#print the even or odd lines ONLY accordingly
 
 def lines_printed_custom(lines_list):
-    if (len(lines_list) % 2) == 0:
+    if (len(lines_list) % 2) == 0: #Determines if text doc is even
         for i in range(len(lines_list)):
-            if i % 2 == 0:
-                print(lines_list[i + 1])
-    else:
+            if i % 2 == 0: #determines which lines are even
+                print(lines_list[i + 1]) #prints the index + 1, because line 2 is index 1 (which is not even)
+    else: # same as above but for odds
         for i in range(len(lines_list)):
-            if i % 2 != = 0:
+            if i % 2 !== 0: 
                 print(lines_list[i])
 
 
-test(lines_list)
+lines_printed_custom(lines_list)
